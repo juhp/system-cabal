0907b89276fc94739c1ef18e8c4329d70dbcd2aa
Jens Petersen <juhpetersen@gmail.com>
2022-01-17 15:41:20 +0800

    rework to check for missing deps; add haddock
    - fails back to cabal-install if Haskell system deps unavailable
    - CabalCmd replaces RunMode (but test param gone for now)
    - needToConfigure: correctly parse system base as PackageId
    - cmdStages and modeOptions gone
    - when reconfiguring check deps
    - now assumes rpm/dnf: need to generalize to support deb, etc
    - use simple-cabal

--- src/Main.hs
+++ src/Main.hs
@@ -5,58 +5,52 @@
 module Main (main) where
 
 import Control.Monad.Extra
-import Data.Either.Extra
+import Data.List ((\\))
 import Data.Maybe
 #if MIN_VERSION_Cabal(3,0,0)
 import Distribution.Parsec (simpleParsec)
 #else
 import Distribution.Text (simpleParse)
 #endif
-import Distribution.Simple (defaultMainArgs, mkPackageName, unPackageName,
-                            pkgName)
+import Distribution.Simple (defaultMainArgs)
 import Distribution.Simple.Configure (tryGetConfigStateFile)
 import Distribution.Simple.Setup (configTests, Flag(..))
 import Distribution.Types.LocalBuildInfo (LocalBuildInfo, configFlags,
                                           installedPkgs, localPkgDescr)
-import Distribution.Types.PackageDescription (package)
+import Distribution.Types.PackageId
+import Distribution.Types.PackageName
 import Distribution.Simple.PackageIndex (lookupPackageName)
+import qualified SimpleCabal as SC
 import SimpleCmd
 import SimpleCmdArgs
 import System.Directory
 import System.FilePath
 import Paths_system_cabal (version)
 
-data RunMode = ConfigCmd | BuildCmd | InstallCmd | TestCmd | ReplCmd | HelpCmd
-
-data CabalCmd = Configure Bool | Build | Install | Test | Repl
+data CabalCmd = Configure | Build | Install | Test | Haddock | Repl | Help
+  deriving Eq
 
 instance Show CabalCmd where
-  show (Configure _) = "configure"
+  show Configure = "configure"
   show Build = "build"
   show Install = "install"
   show Test = "test"
+  show Haddock = "haddock"
   show Repl = "repl"
+  show Help = "help"
 
-getLocalBuildInfo :: IO (Maybe LocalBuildInfo)
-getLocalBuildInfo = do
-  dist <- doesDirectoryExist "dist"
-  if dist
-    then eitherToMaybe <$> tryGetConfigStateFile "dist/setup-config"
-    else return Nothing
-
-getLocalBuildInfo' :: IO LocalBuildInfo
-getLocalBuildInfo' = do
-  dist <- doesDirectoryExist "dist"
-  let setupConfig = "dist/setup-config"
-  if dist
-    then either (error' .show) id <$> tryGetConfigStateFile setupConfig
-    else error' $ setupConfig ++ " not found"
+-- getLocalBuildInfo :: IO (Either ConfigStateFileError LocalBuildInfo)
+-- getLocalBuildInfo = do
+--   dist <- doesDirectoryExist "dist"
+--   if dist
+--     then eitherToMaybe <$> tryGetConfigStateFile "dist/setup-config"
+--     else return Nothing
 
 needToConfigure :: Bool -> IO Bool
 needToConfigure test = do
-  mlbi <- getLocalBuildInfo
-  case mlbi of
-    Just lbi -> do
+  elbi <- tryGetConfigStateFile "dist/setup-config"
+  case elbi of
+    Right lbi -> do
       case lookupPackageName (installedPkgs lbi) (mkPackageName "base") of
         [] -> return True
         [(basever,_)] -> do
@@ -67,42 +61,16 @@ needToConfigure test = do
             simpleParse
 #endif
             <$> cmd "ghc-pkg" ["list", "--simple-output", "base"]
-          if msysbase /= Just basever
-            then return True
-            else do
-            let testsuite = configTests $ configFlags lbi
-            return $ test && testsuite == Flag False
+          case msysbase of
+            Just pkgid ->
+              if pkgid /= PackageIdentifier (mkPackageName "base") basever
+              then return True
+              else do
+                let testsuite = configTests $ configFlags lbi
+                return $ test && testsuite == Flag False
+            Nothing -> return True
         _ -> return True
-    Nothing -> return True
-
-cmdStages :: RunMode -> IO [CabalCmd]
-cmdStages ConfigCmd =
-  return [Configure False]
-cmdStages BuildCmd = do
-  needconfig <- needToConfigure False
-  return $ [Configure False | needconfig] ++ [Build]
-cmdStages InstallCmd = do
-  build <- cmdStages BuildCmd
-  return $ build ++ [Install]
-cmdStages TestCmd = do
-  needconfig <- needToConfigure True
-  return $ [Configure True | needconfig] ++ [Build, Test]
-cmdStages ReplCmd = do
-  needconfig <- needToConfigure True
-  return $ [Configure True | needconfig] ++ [Repl]
-cmdStages HelpCmd =
-  return []
-
-modeOptions :: CabalCmd -> IO [String]
-modeOptions (Configure test) = do
-  home <- getHomeDirectory
-  return $ ["--user","--prefix=" ++ home </> ".local"] ++ ["--enable-tests" | test]
--- lib:name or name, etc
-modeOptions Repl = do
-  lbi <- getLocalBuildInfo'
-  let pkgname = unPackageName . pkgName . package . localPkgDescr
-  return [pkgname lbi]
-modeOptions _ = return []
+    Left err -> print err >> return True
 
 main :: IO ()
 main =
@@ -112,37 +80,112 @@ main =
     [ Subcommand "config" "Configure a package" $
       -- FIXME okay to install libraries?
       -- FIXME --bindir
-      runCmd ConfigCmd
+      runCmd Configure
       <$> optional (strArg "PKG")
     , Subcommand "configure" "alias for config" $
-      runCmd ConfigCmd
+      runCmd Configure
       <$> optional (strArg "PKG")
     , Subcommand "build" "Build a package" $
-      runCmd BuildCmd
+      runCmd Build
       <$> optional (strArg "PKG")
     , Subcommand "install" "Install a package" $
-      runCmd InstallCmd
+      runCmd Install
       <$> optional (strArg "PKG")
     , Subcommand "test" "Test a package" $
-      runCmd TestCmd
+      runCmd Test
+      <$> optional (strArg "PKG")
+    , Subcommand "haddock" "Build documentation" $
+      runCmd Haddock
       <$> optional (strArg "PKG")
     , Subcommand "repl" "Run interpreter" $
-      runCmd ReplCmd
+      runCmd Repl
       <$> optional (strArg "PKG")
     , Subcommand "help" "Help output" $
-      runCmd HelpCmd
+      runCmd Help
       <$> optional (strArg "COMMAND")
     ]
 
-runCmd :: RunMode -> Maybe String -> IO ()
-runCmd HelpCmd marg =
+-- debug :: Bool
+-- debug = True
+
+-- FIXME handle --test
+runCmd :: CabalCmd -> Maybe String -> IO ()
+runCmd Help marg =
   defaultMainArgs (maybeToList marg ++ ["--help"])
 runCmd mode mpkg = do
   findCabalProjectDir mpkg
-  cmds <- cmdStages mode
-  forM_ cmds $ \com -> do
-    options <- modeOptions com
-    defaultMainArgs (show com:options)
+  needconfig <- needToConfigure False
+  if needconfig
+    then do
+    pkgdesc <- SC.findCabalFile >>= SC.readFinalPackageDescription []
+    -- FIXME use packageDependencies
+    let builddeps = SC.buildDependencies pkgdesc
+    missing <- filterM notInstalled builddeps
+    if null missing
+      then do
+      runConfigure False
+      runCabal
+      else do
+      putStrLn "Running repoquery"
+      available <- catMaybes <$> mapM repoqueryGhcDevel missing
+      unless (null available) $
+        installPkgs available
+      let notpackaged = map ghcDevelPkg missing \\ available
+      if null notpackaged
+        then do
+        runConfigure False
+        runCabal
+        else do
+        -- FIXME record missing packages
+        putStrLn $ "Missing system libs:\n" ++ unlines notpackaged
+        putStrLn "Falling back to cabal-install:"
+        cmd_ "cabal" [show mode]
+    else runCabal
+  where
+    runCabal =
+      case mode of
+        Configure -> return ()
+        Repl -> runRepl
+        _ -> defaultMainArgs [show mode]
+
+    runConfigure test = do
+      home <- getHomeDirectory
+      let options' =
+            ["--user","--prefix=" ++ home </> ".local"] ++ ["--enable-tests" | test]
+      defaultMainArgs ("configure":options')
+
+    runRepl = do
+      -- lib:name or name, etc
+      lbi <- getLocalBuildInfo'
+      let pkgname = unPackageName . pkgName . SC.package . localPkgDescr
+          options' = [pkgname lbi]
+      defaultMainArgs ("repl":options')
+
+getLocalBuildInfo' :: IO LocalBuildInfo
+getLocalBuildInfo' = do
+  dist <- doesDirectoryExist "dist"
+  let setupConfig = "dist/setup-config"
+  if dist
+    then either (error' . show) id <$> tryGetConfigStateFile setupConfig
+    else error' $ setupConfig ++ " not found"
+
+-- FIXME support deb too
+notInstalled :: PackageName -> IO Bool
+notInstalled dep =
+  not <$> cmdBool "rpm" ["-q", "--quiet", "--whatprovides", ghcDevelPkg dep]
+
+ghcDevelPkg :: PackageName -> String
+ghcDevelPkg dep =
+  "ghc-" ++ unPackageName dep ++ "-devel"
+
+repoqueryGhcDevel :: PackageName -> IO (Maybe String)
+repoqueryGhcDevel dep = do
+  res <- sudo "dnf" ["repoquery", "--qf", "%{name}", ghcDevelPkg dep]
+  return $ if null res then Nothing else Just res
+
+installPkgs :: [String] -> IO ()
+installPkgs pkgs =
+  sudo_ "dnf" ("install":pkgs)
 
 -- adapted from stack-all findStackProjectDir
 findCabalProjectDir :: Maybe FilePath -> IO ()
